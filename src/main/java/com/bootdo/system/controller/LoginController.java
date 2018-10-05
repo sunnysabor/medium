2dc20f48de4745d86139e4f9cd65bb6c38f7e83e
chenchen <chenchen@hd123.com>
2018-10-05 17:40:39 +0800

    首页

--- src/main/java/com/bootdo/system/controller/LoginController.java
+++ src/main/java/com/bootdo/system/controller/LoginController.java
@@ -27,81 +27,81 @@ import java.util.List;
 
 @Controller
 public class LoginController extends BaseController {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    MenuService menuService;
-    @Autowired
-    FileService fileService;
+  @Autowired
+  MenuService menuService;
+  @Autowired
+  FileService fileService;
 
-    @GetMapping({"/", ""})
-    String welcome(Model model) {
-        return "redirect:/login";
-    }
+  @GetMapping({ "/", "" })
+  String welcome(Model model) {
+    return "/guest/guest";
+  }
 
-    @Log("请求访问主页")
-    @GetMapping({"/index"})
-    String index(Model model) {
-        List<Tree<MenuDO>> menus = menuService.listMenuTree(getUserId());
-        model.addAttribute("menus", menus);
-        model.addAttribute("name", getUser().getName());
-        FileDO fileDO = fileService.get(getUser().getPicId());
-        if ("admin".equals(getUser().getIdentity())) {
-            //返回后台
-            if (fileDO != null && fileDO.getUrl() != null) {
-                if (fileService.isExist(fileDO.getUrl())) {
-                    model.addAttribute("picUrl", fileDO.getUrl());
-                } else {
-                    model.addAttribute("picUrl", "/img/photo_s.jpg");
-                }
-            } else {
-                model.addAttribute("picUrl", "/img/photo_s.jpg");
-            }
-            model.addAttribute("username", getUser().getUsername());
-            return "index_v1";
-        } else if ("user".equals(getUser().getIdentity())) {
-            //返回前台
-            model.addAttribute("username", getUser().getUsername());
-            return "/user/index/main";
+  @Log("请求访问主页")
+  @GetMapping({ "/index" })
+  String index(Model model) {
+    List<Tree<MenuDO>> menus = menuService.listMenuTree(getUserId());
+    model.addAttribute("menus", menus);
+    model.addAttribute("name", getUser().getName());
+    FileDO fileDO = fileService.get(getUser().getPicId());
+    if ("admin".equals(getUser().getIdentity())) {
+      //返回后台
+      if (fileDO != null && fileDO.getUrl() != null) {
+        if (fileService.isExist(fileDO.getUrl())) {
+          model.addAttribute("picUrl", fileDO.getUrl());
         } else {
-            //do nothing
-            return null;
+          model.addAttribute("picUrl", "/img/photo_s.jpg");
         }
+      } else {
+        model.addAttribute("picUrl", "/img/photo_s.jpg");
+      }
+      model.addAttribute("username", getUser().getUsername());
+      return "index_v1";
+    } else if ("user".equals(getUser().getIdentity())) {
+      //返回前台
+      model.addAttribute("username", getUser().getUsername());
+      return "/user/index/main";
+    } else {
+      //do nothing
+      return null;
     }
+  }
 
-    @GetMapping("/login")
-    String login() {
-        return "login";
-    }
+  @GetMapping("/login")
+  String login() {
+    return "login";
+  }
 
-    @Log("登录")
-    @PostMapping("/login")
-    @ResponseBody
-    R ajaxLogin(String username, String password) {
-        password = MD5Utils.encrypt(username, password);
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-        Subject subject = SecurityUtils.getSubject();
-        try {
-            subject.login(token);
-            return R.ok();
-        } catch (AuthenticationException e) {
-            return R.error("用户或密码错误");
-        }
+  @Log("登录")
+  @PostMapping("/login")
+  @ResponseBody
+  R ajaxLogin(String username, String password) {
+    password = MD5Utils.encrypt(username, password);
+    UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+    Subject subject = SecurityUtils.getSubject();
+    try {
+      subject.login(token);
+      return R.ok();
+    } catch (AuthenticationException e) {
+      return R.error("用户或密码错误");
     }
+  }
 
-    @GetMapping("/logout")
-    String logout() {
-        ShiroUtils.logout();
-        return "redirect:/login";
-    }
+  @GetMapping("/logout")
+  String logout() {
+    ShiroUtils.logout();
+    return "redirect:/login";
+  }
 
-    @GetMapping("/main")
-    String main() {
-        return "main";
-    }
+  @GetMapping("/main")
+  String main() {
+    return "main";
+  }
 
-    public static void main(String[] args) {
-        String pwd = MD5Utils.encrypt("test", "test");
-        System.out.println(pwd);
-    }
+  public static void main(String[] args) {
+    String pwd = MD5Utils.encrypt("test", "test");
+    System.out.println(pwd);
+  }
 }
